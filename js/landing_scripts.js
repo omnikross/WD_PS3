320d9ee9bb31a6cfe16d1f81c1066b775098470f
Maksim Byakov <bms1200@gmail.com>
2017-05-08 16:16:39 +0300

    All .js are here.

---
+++ js/landing_scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $(".scroller").fadeIn();
+        } else {
+            $(".scroller").fadeOut();
+        }
+    });
+
+    $(".scroller").click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
+    $("#header").on("click", "button", function() {
+        var id = $(this).attr("href");
+        var top = $(id).offset().top;
+        var heightOffset = ($(window).height() - $(id).height()) / 2;
+        if (heightOffset < 0) {
+            heightOffset = 0;
+        } 
+        var centerTop = top - heightOffset; 
+        $('body,html').animate({scrollTop: centerTop}, 1500);
+    });
+});
+
+
+
---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+	var contacts = ["Jenny Hess", "Elliot Fu", "Stevie Feliciano","Christian", "Matt"];
+	$(".wrapperDropdown").append('<ul>');
+	$(".wrapperDropdown ul").append('<span>');
+	
+	for (var i = 0; i < contacts.length; i++) {
+		$(".wrapperDropdown ul").append('<li><a>' + contacts[i] + '</a></li>');
+	}
+
+	$(".wrapperDropdown span").addClass("dropdown").text("Select Friend").prependTo(".wrapperDropdown ul");
+	$(".wrapperDropdown ul").addClass("list");
+	$(".list").click(function() {
+		$(".wrapperDropdown li").toggle(300);
+	})
+	$(".wrapperDropdown li").click(function() {
+		$(".dropdown").html($(this).text())
+	});
+});
